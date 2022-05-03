74d9fca4f6467009f3c657cd5ea2877c932d4900
John Cassidy <jpcassidy@outlook.com>
2022-05-03 10:17:25 -0400

    add AdertApi Nuget package

---
+++ AWSDotNetWebAdvert.Web/ServiceClients/IAdvertApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+
+namespace AWSDotNetWebAdvert.Web.ServiceClients
+{
+    public interface IAdvertApiClient
+    {
+        Task<CreateAdvertResponse> Create(AdvertModel model);
+        //Task<AdvertResponse> CreateAsync(CreateAdvertModel model);
+        //Task<bool> ConfirmAsync(ConfirmAdvertRequest model);
+        //Task<List<Advertisement>> GetAllAsync();
+        //Task<Advertisement> GetAsync(string advertId);
+    }
+}
