8f30fc922bdd8ed62cb0728ae237d11801acdb96
John Cassidy <jpcassidy@outlook.com>
2022-05-06 08:13:38 -0400

    add login/signout links to header nav

--- AWSDotNetWebAdvert.Web/Controllers/AccountsController.cs
+++ AWSDotNetWebAdvert.Web/Controllers/AccountsController.cs
@@ -92,5 +92,11 @@ namespace AWSDotNetWebAdvert.Web.Controllers {
 
             return View("Login", model);
         }
+
+        public async Task<IActionResult> Signout() {
+            if (User.Identity.IsAuthenticated)
+                await _signInManager.SignOutAsync().ConfigureAwait(false);
+            return RedirectToAction("Login");
+        }
     }
 }
